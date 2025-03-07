044daac496553dcef458c25dcd67cf76fbcef3ac
hrh <761512439@qq.com>
2025-03-07 17:59:35 +0800

    fix: 🧩 common request type passing、Request return file processing、Configuration file change

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 /*
- * @Author: hrh 
- * @Date: 2023-03-24 16:24:04 
+ * @Author: hrh
+ * @Date: 2023-03-24 16:24:04
  * @Last Modified by: hrh
  * @Last Modified time: 2023-04-18 18:57:25
  */
@@ -11,68 +11,72 @@ import { ResultEnum, RequestEnum } from "@/enums/httpEnum";
  * @param path 路径
  * @param data 请求数据
  */
-const createKey = (path: string, data: any) => `${JSON.stringify(data)}${path}`
+const createKey = (path: string, data: any) => `${JSON.stringify(data)}${path}`;
 
 //储存request请求map
-const requestList: Map<string, UniApp.RequestTask> = new Map()
+const requestList: Map<string, UniApp.RequestTask> = new Map();
+let baseUrl: string = import.meta.env.VITE_APP_BASE_PREFIX;
 // 获取域名
-const baseUrl = import.meta.env.VITE_APP_BASE_URL
+if (import.meta.env.VITE_USER_NODE_ENV === "production") baseUrl += import.meta.env.VITE_APP_BASE_URL;
 const requestHttp = async <T>(options: UniApp.RequestOptions): Promise<ResultData<T>> => {
   uni.showLoading({
     title: "加载中",
-    mask: true,
-  })
-  var provider = ''
+    mask: true
+  });
   return new Promise((resolve, reject) => {
     //生成key
-    const requestKey = createKey(options.url, options.data)
+    const requestKey = createKey(options.url, options.data);
     const requestClose = uni.request({
-      url: `${baseUrl}${provider}${options.url}`,
-      header: {
-        ...options.header,
-      },
-      method: options.method,
+      ...options,
+      url: `${baseUrl}${options.url}`,
       timeout: options.timeout || 6000,
-      data: options.data,
       success: (res: any) => {
         // 请求成功后处理逻辑
-        uni.hideLoading()
-        if (res?.data?.code === ResultEnum.SUCCESS) {
-          resolve(res.data)
+        uni.hideLoading();
+        if (options.responseType) {
+          // 可转base64
+          // const base64 = uni.arrayBufferToBase64(res.data);
+          // const data: any = `data:image/png;base64,${base64}`;
+          resolve({
+            code: 1,
+            data: res.data,
+            msg: "ok"
+          });
+        } else if (res?.data?.code === ResultEnum.SUCCESS) {
+          resolve(res.data);
         } else if (res?.data?.code === ResultEnum.OVERDUE) {
           uni.showToast({
-            title: res.data.message || '登录过期',
-            icon: 'none'
-          })
+            title: res.data.message || "登录过期",
+            icon: "none"
+          });
         } else {
           uni.showToast({
-            title: res.data.message || '服务错误',
-            icon: 'none',
+            title: res.data.message || "服务错误",
+            icon: "none",
             duration: 2500
-          })
-          reject(res?.data)
+          });
+          reject(res?.data);
         }
       },
-      fail: (err) => {
-        uni.hideLoading()
-        reject(err)
+      fail: err => {
+        uni.hideLoading();
+        reject(err);
       },
       complete: () => {
-        requestList.delete(requestKey)
-
+        requestList.delete(requestKey);
       }
-    })
+    });
     //先关闭上一个同路径同参数请求
-    requestList.get(requestKey)?.abort()
+    requestList.get(requestKey)?.abort();
     //存储请求
-    requestList.set(requestKey, requestClose)
-  })
-}
+    requestList.set(requestKey, requestClose);
+  });
+};
 export default {
-  GET: <T>(options: { url: string, data?: object }): Promise<ResultData<T>> => {
-    return requestHttp({ url: options.url, method: RequestEnum.GET, data: options.data })
+  GET: <T>(options: UniApp.RequestOptions): Promise<ResultData<T>> => {
+    return requestHttp({ ...options, method: RequestEnum.GET });
   },
-  POST: <T>(options: { url: string, data?: object }): Promise<ResultData<T>> => {
-    return requestHttp({ url: options.url, method: RequestEnum.POST, data: options.data })
+  POST: <T>(options: UniApp.RequestOptions): Promise<ResultData<T>> => {
+    return requestHttp({ ...options, method: RequestEnum.POST });
   }
-} 
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
   resolve: {
-    alias: [{ find: "@", replacement: './src' }]
+    alias: [{ find: "@", replacement: "./src" }]
   },
   css: {
     preprocessorOptions: {
-      scss: {
-
+      scss: {}
+    }
+  },
+  // 代理
+  server: {
+    port: 8080,
+    // https: true,
+    //disabledHostCheck:true,
+    proxy: {
+      "/base": {
+        target: loadEnv("development", "./").VITE_APP_BASE_URL,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/base/, "/base")
       }
     }
   }
